7a86e53be687c2938a657e6ccc8ff172e1e2643b
kyomi <contato.kyomi@gmail.com>
2022-04-06 00:09:50 -0300

    Bump to v0.1.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     project_urls={
         'Issue tracker': 'https://github.com/soukyomi/jaskier/issues',
     },
-    version='0.1.1',
+    version='0.1.2',
     packages=packages,
     license='MIT',
     description='Library to make loggings more prettier and readable.',
